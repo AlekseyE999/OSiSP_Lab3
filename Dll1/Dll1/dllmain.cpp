aff6cf8fb118b1760ea3eab0df14b9ffe4835750
Aleksey Edunov <alekseye333@gmail.com>
2021-11-14 21:11:11 +0300

    Add  usingStaticDLL

--- Dll1/Dll1/dllmain.cpp
+++ Dll1/Dll1/dllmain.cpp
@@ -1,15 +1,45 @@
-﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
-#include "pch.h"
+﻿#include "pch.h"
+#include <chrono>
+#include <fstream>
+#include <TlHelp32.h>
+#include <vector>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+#include "Dll.h"
+
+DWORD GetProcessIdByName(const char* processName)
+{
+    DWORD processId = 0;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 processEntry;
+    ZeroMemory(&processEntry, sizeof(processEntry));
+    processEntry.dwSize = sizeof(processEntry);
+    std::vector<std::string> vd;
+    while (Process32Next(hSnapshot, &processEntry))
+    {
+
+        if (!strcmp(processEntry.szExeFile, processName))
+        {
+            processId = processEntry.th32ProcessID;
+            break;
+        }
+
+        vd.push_back(processEntry.szExeFile);
+    }
+
+    return processId;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
+        ReplaceString(GetProcessIdByName("ProcessToInjectInto.exe"), "Hello World!", "Hello dddsdd");
+        break;
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
---
+++ Dll1/UsingStaticDll/UsingStaticDll.cpp
@@ -0,0 +1,40 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+#include <vector>
+
+#include "DLL.h"
+
+DWORD GetProcessIdByName(const char* processName)
+{
+	DWORD processId = 0;
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 processEntry;
+	ZeroMemory(&processEntry, sizeof(processEntry));
+	processEntry.dwSize = sizeof(processEntry);
+	std::vector<std::string> vd;
+	while (Process32Next(hSnapshot, &processEntry))
+	{
+
+		if (!strcmp(processEntry.szExeFile, processName))
+		{
+			processId = processEntry.th32ProcessID;
+			break;
+		}
+
+		vd.push_back(processEntry.szExeFile);
+	}
+
+	return processId;
+}
+
+int main()
+{
+	std::string oldString = "Hello";
+	std::string oldString1 = "world";
+	std::string newString = "Hi danik";
+	std::cout << "Start \"oldString\" value: " << oldString1 << std::endl;
+	ReplaceString(GetProcessIdByName("UsingStaticDll.exe"), oldString.c_str(), newString.c_str());
+	std::cout << "End \"oldString\" value: " << oldString1 << std::endl;
+	return 0;
+}
